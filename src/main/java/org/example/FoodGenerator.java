720629f5c095ec508482ad18e0839a5fb5aaafb1
Maja21472 <wwx15367@student.wsb.warszawa.pl>
2023-06-26 10:13:13 +0200

    utworzenie FoodGeneratora z pierwszym przepisem

---
+++ src/main/java/org/example/FoodGenerator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class FoodGenerator {
+    Recipe findRecipe(List<String> ingredients) {
+        Recipe recipe = new Recipe("naleśniki");
+        return recipe;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FoodGenerator foodGenerator = new FoodGenerator();
+        List<String> ingredients ("jajka", "mleko", "woda", "mąka", "sól");
+        foodGenerator.findRecipe(ingredients);
     }
 }
