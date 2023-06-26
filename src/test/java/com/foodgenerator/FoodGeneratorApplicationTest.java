14228181c5a9e12d6a7b94d8d4f724ad6e47d100
Maja21472 <wwx15367@student.wsb.warszawa.pl>
2023-06-26 14:13:30 +0200

    stworzenie testu cd

--- src/test/java/com/foodgenerator/FoodGeneratorApplicationTest.java
+++ src/test/java/com/foodgenerator/FoodGeneratorApplicationTest.java
@@ -2,9 +2,24 @@ package com.foodgenerator;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 public class FoodGeneratorApplicationTest {
     @Test
     public void should_return_recipe_after_giving_5correct_ingredients(){
+        FoodGeneratorResult result = new FoodGeneratorApplicationStrater().start();
+        assertThat(result.message()).isEqualTo("Udało się:)");
+        assertThat(result.recipe()).isEqualTo("naleśniki");
+
+
+    }
+
+    @Test
+    public void should_return_error_after_giving_morethan5correct_ingredients(){
+        FoodGeneratorResult  start = new FoodGeneratorApplicationStrater().start();
+    }
+
+    @Test
+    public void should_return_error_after_giving_lessthan5correct_ingredients(){
         FoodGeneratorResult  start = new FoodGeneratorApplicationStrater().start();
     }
 }
